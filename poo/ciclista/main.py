e5524557cb15b87a16ca55ab82c0a8cbdd3846b2
Jose Herrera <hjose0650@gmail.com>
2022-04-13 20:25:32 -0500

    Realizamos el punto del ciclista

--- poo/ciclista/main.py
+++ poo/ciclista/main.py
@@ -1,56 +1,53 @@
 from Ciclista import Ciclista
-
+#El programa debe mostrar el ciclista con el mejor tiempo
 ciclistas1 = Ciclista()
 ciclistas2 = Ciclista()
-nombre1=input("Digite el nombre del ciclista: ")
-edad1=int(input("Digite la edad del ciclista: "))
-pais1=input("Digite el pais del ciclista: ")
-tiempo1=int(input("Digite el tiempo que le tomó la prueba: "))
-ciclistas1 = Ciclista.__nombre=nombre1
-ciclistas1 = Ciclista.__edad=edad1
-ciclistas1 = Ciclista.__pais=pais1
-ciclistas1 = Ciclista.__tiempo=tiempo1
-nombre2=input("Digite el nombre del segundo ciclista ciclista: ")
-edad2=int(input("Digite la edad del segundo ciclista ciclista: "))
-pais2=input("Digite el pais del segundo ciclista ciclista: ")
-tiempo2=int(input("Digite el tiempo que le tomó la prueba al segundo ciclista: "))
-ciclistas2 = Ciclista.__nombre=nombre2
-ciclistas2 = Ciclista.__edad=edad2
-ciclistas2 = Ciclista.__pais=pais2
-ciclistas2 = Ciclista.__tiempo=tiempo2
-# while (opcion !=0):
-#     print("Digitar 1 para Ingresar un ciclista (nombre,edad,pais y tiempo):  ")
-#     print("Digitar 2 para mostrar todos los ciclistas:  ")
-#     print(" Digitar 0 para SALIR:  ")
-#     opcion=int(input("Digita una opcion: "))
-#     if(opcion == 0):
-#         break
-#     elif(opcion==1):
-#             nombre1=input("Digite el nombre del ciclista: ")
-#             edad1=int(input("Digite la edad del ciclista: "))
-#             pais1=input("Digite el pais del ciclista: ")
-#             tiempo1=int(input("Digite el tiempo que le tomó la prueba: "))
-#             ciclista1 = Ciclista.nombre=nombre1
-#             ciclista1 = Ciclista.edad=edad1
-#             ciclista1 = Ciclista.pais=pais1
-#             ciclista1 = Ciclista.tiempo=tiempo1
-#             nombre2=input("Digite el nombre del segundo ciclista ciclista: ")
-#             edad2=int(input("Digite la edad del segundo ciclista ciclista: "))
-#             pais2=input("Digite el pais del segundo ciclista ciclista: ")
-#             tiempo2=int(input("Digite el tiempo que le tomó la prueba al segundo ciclista: "))
-#             ciclista2 = Ciclista.nombre=nombre2
-#             ciclista2 = Ciclista.edad=edad2
-#             ciclista2 = Ciclista.pais=pais2
-#             ciclista2 = Ciclista.tiempo=tiempo2
-#     # elif(opcion==2):
-#     #     print(ciclistas1.nombre)
-#     #     print(ciclistas1.tiempo)
-#     #     print(ciclistas2.nombre)
-#     #     print(ciclistas2.tiempo)
-#     else:
-#         print("el numero que ingresaste no esta en el menu")
+opcion = 1
+while (opcion !=0):
+    print("Digitar 1 para Ingresar dos ciclistas (nombre,edad,pais y tiempo)")
+    print("Digitar 2 para mostrar todos los ciclistas")
+    print("Digitar 3 para mostrar al mejor tiempo")
+    print(" Digitar 0 para SALIR")
+    opcion=int(input("Digita una opcion: "))
+    if(opcion == 0):
+        break
+    elif(opcion==1):
 
-print(ciclistas1.__nombre)
-print(ciclistas1.__tiempo)
-print(ciclistas2.__nombre)
-print(ciclistas2.__tiempo)
+            nombre1=input("Digite el nombre del ciclista: ")
+            edad1=int(input("Digite la edad del ciclista: "))
+            pais1=input("Digite el pais del ciclista: ")
+            tiempo1=int(input("Digite el tiempo que le tomó la prueba: "))
+            ciclistas1.nombre=nombre1
+            ciclistas1.edad=edad1
+            ciclistas1.pais=pais1
+            ciclistas1.tiempo=tiempo1
+
+            nombre2=input("Digite el nombre del segundo ciclista ciclista: ")
+            edad2=int(input("Digite la edad del segundo ciclista ciclista: "))
+            pais2=input("Digite el pais del segundo ciclista ciclista: ")
+            tiempo2=int(input("Digite el tiempo que le tomó la prueba al segundo ciclista: "))
+            ciclistas2.nombre=nombre2
+            ciclistas2.edad=edad2
+            ciclistas2.pais=pais2
+            ciclistas2.tiempo=tiempo2       
+    elif(opcion==2):
+        print(ciclistas1.nombre)
+        print(ciclistas1.tiempo)
+        print(ciclistas2.nombre)
+        print(ciclistas2.tiempo)
+    elif(opcion==3):
+        primerTiempo = ciclistas1.tiempo
+        segundoTiempo = ciclistas2.tiempo
+        if(primerTiempo<segundoTiempo):
+            print(ciclistas1.nombre)
+            print(ciclistas1.edad)
+            print(ciclistas1.pais)
+            print(ciclistas1.tiempo)
+        else:
+            print(ciclistas2.nombre)
+            print(ciclistas2.edad)
+            print(ciclistas2.pais)
+            print(ciclistas2.tiempo)
+
+    else:
+        print("el numero que ingresaste no esta en el menu")
